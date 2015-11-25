eecf9e6c577e0ed50f0593904d56e70227219812
lcdonaldson <lcdbacanas@gmail.com>
2015-11-24 20:03:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // the following has nothing to do with puppy love its just practice from another lesson
 
-// document.getElementById('id') // returns single element if found
-// document.getElementsByClassName('class') // returns multiple elements if found
-// document.getElementByTagName('div') // returns multiple elements if found
+/* document.getElementById('id') // returns single element if found
+* document.getElementsByClassName('class') // returns multiple elements if found
+* document.getElementByTagName('div') // returns multiple elements if found
 
-// document.querySelector('div .foo > a') // single element if found
-// document.querySelectorAll('.hero') // all matching elements
+* document.querySelector('div .foo > a') // single element if found
+* document.querySelectorAll('.hero') // all matching elements */
 
 
 
